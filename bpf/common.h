b4801166ebc3ae3d17f7c1592dc7def0426c5166
Ghassan Malke <ghassan+github@malke.nl>
2026-02-15 15:31:10 +0100

    fix: DaemonSet no-events and systemd cgroup driver

--- bpf/common.h
+++ bpf/common.h
@@ -14,6 +14,18 @@ typedef unsigned int __u32;
 typedef int __s32;
 typedef unsigned long long __u64;
 typedef long long __s64;
+typedef __u16 __be16;
+typedef __u32 __be32;
+typedef __u64 __be64;
+typedef __u32 __wsum;
+typedef __s8 s8;
+typedef __u8 u8;
+typedef __s16 s16;
+typedef __u16 u16;
+typedef __s32 s32;
+typedef __u32 u32;
+typedef __s64 s64;
+typedef __u64 u64;
 #endif
 
 #include <bpf/bpf_helpers.h>
